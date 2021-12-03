c8dc21337f7f67310312db4ec25679f515718f26
Scott Eppler <seppler@presidio.com>
2021-12-03 10:29:44 -0600

    Cleanup Day3 Solution 1
    Use list comprehension for efficiency

--- 2021/Day3/binary_diagnostic_1.py
+++ 2021/Day3/binary_diagnostic_1.py
@@ -3,13 +3,7 @@ with open('input.txt','r') as input:
 
 diag_arr = [[line[pos] for line in diag_report] for pos in range(len(diag_report[0]))]
 
-bin_gamma = ''
-bin_epsilon = ''
-for pos in diag_arr:
-  bin_gamma += max(set(pos), key = pos.count)
-  bin_epsilon += min(set(pos), key = pos.count)
-
-gamma = int(bin_gamma,2)
-epsilon = int(bin_epsilon,2)
+gamma = int(''.join([max(set(pos), key = pos.count) for pos in diag_arr]),2)
+epsilon = int(''.join([min(set(pos), key = pos.count) for pos in diag_arr]),2)
 
 print(f"Power Consumtion: {gamma * epsilon}")
